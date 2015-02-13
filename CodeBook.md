ab9d5b7982652773dd8f4ec91d285c34ce77e5a0
jrherrick <jrherrick@users.noreply.github.com>
2015-02-13 09:23:55 -0800

    first ci

---
+++ CodeBook.md
@@ -0,0 +1,10 @@
+# Code Book
+*for Assignment Feb 22, 2015*
+
+Code book for getdata-011 assignment Feb 22, 2015, satisfying item 3) "a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md"
+
+## Variables
+
+## Data
+
+## Transformations
---
+++ run-analysis.R
@@ -0,0 +1,19 @@
+# run-analysis.R
+
+
+# Merges the training and the test sets to create one data set. -----------
+
+
+# Extracts mean and standard deviation for each measure -------------------
+
+
+# Use descriptive activity names to name the activities -------------------
+
+
+# Labels the data set with descriptive variable names.  -------------------
+
+
+# tidy data set with the average of each variable -------------------------
+# for each activity and each subject. -------------------------------------
+
+
